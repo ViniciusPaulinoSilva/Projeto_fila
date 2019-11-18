8d99b9b2f341dc23faf65b53602e16af7c6c2bde
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2019-11-18 12:39:14 -0200

    Função de inserir

--- Start.c
+++ Start.c
@@ -1,3 +1,9 @@
+/******************************************************
+ *                                                    *
+ *   Nomes: Gabriel Yamamoto, Vinicius Silva          *
+ *             RAs: 19241611, 18...                   *
+ *                                                    *
+ ******************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,42 +19,52 @@ typedef struct fila no;
 
 struct fila
 {
-    time_t criado;
-    int count;
-    struct fila *prox;
+  time_t criado;
+  int count;
+  struct fila *prox;
 };
+
+void insere(no *final)
+{
+  time(&final -> criado);
+  (final -> count)++;
+}
 
 int main()
 {
-    setlocale(LC_ALL, "portuguese");
-    no *fila1 = (no*)malloc(sizeof(no));
-    no *fila2 = (no*)malloc(sizeof(no));
-    no *fila3 = (no*)malloc(sizeof(no));
-    time_t duration;
-    time_t insert;
-    time_t remov;
-    time_t inicio;
-    no *inicio1 = fila1;
-    no *fim1 = fila1;
-    no *inicio2 = fila2;
-    no *fim2 = fila2;
-    no *inicio3 = fila3;
-    no *fim3 = fila3;
-    int media_geral = 0
-    int min = 9999;
-    int max = 0;
+  setlocale(LC_ALL, "portuguese");
+  no *fila1;
+  no *fila2;
+  no *fila3;
+  time_t duration;
+  time_t insert;
+  time_t remov;
+  time_t inicio;
+  no *inicio1 = fila1;
+  no *fim1 = fila1;
+  no *inicio2 = fila2;
+  no *fim2 = fila2;
+  no *inicio3 = fila3;
+  no *fim3 = fila3;
+  int media_geral = 0;
+  int min = 9999;
+  int max = 0;
 
-    while(difftime(duration,inicio!=0)){
-        if(duration%insert==0){
-            //chama função de inserir na menor fila
-        }
-        if(duration%remov==0){
-            //chama função de remover;
-            //soma tempo em variavel media_geral;
-            // verifica se o tempo é menor ou maior q a max e a min;
-        }
-        Sleep(1000);
-        //printa filas e tempo q o programa esta sendo executado
+  while(difftime(duration, inicio) != 0)
+  {
+    if(duration % insert == 0)
+    {
+      //chama função de inserir na menor fila
     }
-    //printa tempos de espera
+    if(duration % remov == 0)
+    {
+      //chama função de remover;
+      //soma tempo em variavel media_geral;
+      // verifica se o tempo é menor ou maior q a max e a min;
+    }
+    Sleep(1000);
+    time(&duration);
+    //printa filas e tempo q o programa esta sendo executado
+  }
+  //printa tempos de espera
 }
